927ec8889b14566221ccd6eee6e39edd23320543
EC2 Default User <ec2-user@ip-10-0-9-166.ap-south-1.compute.internal>
2022-12-05 05:45:31 +0000

    done

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo service docker status
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo docker  -v
+sudo docker pull jenkins/jenkins
+sudo docker images
+sudo mkdir jenkins
+sudo docker run -d --name jenkins -p 8080:8080 -v $PWD/jenkins/ jenkins/jenkins
+sudo docker ps
+sudo docker logs jenkins
